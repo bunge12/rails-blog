a8876dc12204e4c21efaba239a937ecff9562507
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-31 14:18:02 -0700

    Delete functionality added and 'Comment' model added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
 
 def edit
   @article = Article.find(params[:id])
-  
 end
 
   def create
@@ -33,6 +32,13 @@ end
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
    
   private
